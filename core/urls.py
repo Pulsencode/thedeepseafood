e35b271ca4af4fd83207908c627383768cac2ad8
Abhimanue Rajesh <abhimanuemvk@gmail.com>
2025-04-15 15:38:03 +0530

    added publi_interface directory for static files

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,8 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.conf.urls.static import static
-
-
-# from django.views.generic.base import TemplateView
 from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
@@ -19,5 +16,7 @@ urlpatterns = [
     path("products/", include("products.urls")),
     path("company/", include("company.urls")),
 ]
+
+# NOTE Keep this line at the end of the file to serve static files in development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
