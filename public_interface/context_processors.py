8ef01574e698d3f88f7dd9887cb644cff74e6706
sinan kodur <sinankodur24@gmail.com>
2025-05-03 11:47:00 +0530

    blog context processor added and page titles passed as from the webpage

--- public_interface/context_processors.py
+++ public_interface/context_processors.py
@@ -1,6 +1,11 @@
 from datetime import datetime
+from company.models import Blog
 
 
 def company_total_year(request):
     years_elapsed = datetime.now().year - 1986
     return {"years_elapsed": years_elapsed}
+
+
+def blogs(request):
+    return {"all_blogs": Blog.objects.filter(status=True)}
