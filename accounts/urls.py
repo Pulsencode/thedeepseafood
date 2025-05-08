33b5fe051389fdb9afa7291633249ddc6490d05f
sinan kodur <sinankodur24@gmail.com>
2025-05-08 18:16:48 +0530

    change password issue solved

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,20 +1,11 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("profile", views.ProfileView.as_view(), name="profile_view"),
     path(
         "change-password/",
-        auth_views.PasswordChangeView.as_view(
-            template_name="superadmin/profile/profile.html"
-        ),
-        name="password_change",
-    ),
-    path(
-        "change-password/done/",
-        auth_views.PasswordChangeDoneView.as_view(),
-        name="password_change_done",
+        views.ChangePasswordView.as_view(), name="password_change",
     ),
     path("logout", views.LogoutView.as_view(), name="logout_view"),
 ]
