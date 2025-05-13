5d08dc42398afb90aaf07fb0172b53782cba92cb
Abhinav <AbstractProgrammer@example.com>
2025-05-13 14:29:13 +0530

    remove unwanted urls(public interface)

--- public_interface/urls.py
+++ public_interface/urls.py
@@ -32,26 +32,17 @@ urlpatterns = [
         "terms-and-conditions/", views.terms_and_condition, name="terms_and_conditions"
     ),
     path(
-        "get_product_index/", views.IndexProductView.as_view(), name="get_product_index"
-    ),
-    path(
-        "get_product_lists/",
-        views.ProductListingView.as_view(),
-        name="get_product_lists",
-    ),
-    path("search_product/", views.SearchProductView.as_view(), name="search_product"),
-    path(
-        "product_details_switch/",
-        views.GeneralProductDetailsView.as_view(),
-        name="product_details_switch",
-    ),
-    path("brands-list", views.BrandsListView.as_view(), name="brands_list"),
-    path(
         "get_product_content/",
         views.ProductContentView.as_view(),
         name="get_product_content",
     ),
-    path("load_more_product/", views.load_more_product, name="load_more_product"),
+    path(
+        "promotion-view/<int:id>/",
+        views.PromotionView.as_view(),
+        name="promotion_details",
+    ),
+    path("enquiry_email/", views.ProductEnquiryView.as_view(), name="enquiry_email"),
+    # oceano related url
     path(
         "get_product_details/",
         views.OceanoProductDetailsView.as_view(),
@@ -62,19 +53,14 @@ urlpatterns = [
         views.OceanoModalProductDetailsView.as_view(),
         name="get_modal_product_details",
     ),
+    # load pages
+    path("load_more_product/", views.load_more_product, name="load_more_product"),
     path("get_news/", views.LoadNews.as_view(), name="get_news"),
     path("get_global_news/", views.LoadGlobalNews.as_view(), name="get_global_news"),
-    path("load_more_news/", views.load_more_news, name="load_more_news"),
+    # path("load_more_news/", views.load_more_news, name="load_more_news"),
     path("load_more_events/", views.load_more_events, name="load_more_events"),
     path("get_events/", views.LoadEvents.as_view(), name="get_events"),
     path("get_promotions/", views.LoadPromotions.as_view(), name="get_promotions"),
-    path("news-lists", views.NewsListsView.as_view(), name="news_lists"),
-    path(
-        "promotion-view/<int:id>/",
-        views.PromotionView.as_view(),
-        name="promotion_details",
-    ),
-    # path("send_email/", views.SendEmailView.as_view(), name="send_email"),
-    # path("job_email/", views.CareerEmailView.as_view(), name="job_email"),
+    path("load_more_news/", views.load_more_news, name="load_more_news"),
     path("enquiry_email/", views.ProductEnquiryView.as_view(), name="enquiry_email"),
 ]
